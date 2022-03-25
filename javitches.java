daceef5c115701695fb930ba5b2bbd6c5a359d37
Rubyboat1207 <52091447+Rubyboat1207@users.noreply.github.com>
2022-03-25 09:14:41 -0500

    Create javitches.java

---
+++ javitches.java
@@ -0,0 +1,26 @@
+package me.getbitches.javitches;
+
+import amazing.aahana.bitches;
+
+public class Bitches {
+ public void GetBitches()
+ {
+     public void GetBitches()
+  {
+    return null;
+  }
+
+  public Bitches fairMadensTest(String name)
+  {
+    if(Bitches.bitchList.Contains(name))
+    {
+      try{
+          Bitches.bitchlist.getIndex(name).stab();
+      }catch
+      {
+         return Bitches.bitchlist[Bitches.bitchlist.getIndex(name)];
+      }
+    }
+  }
+ }
+}
